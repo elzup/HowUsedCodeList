48ad85e18cb7b8194a26372a91d3a9f983d8031d
elzup <guild0105@gmail.com>
2023-05-22 17:14:16 +0900

    make serach script

--- main.ts
+++ main.ts
@@ -1,11 +1,33 @@
 import { Octokit } from "@octokit/rest";
-const octokit = new Octokit();
 
-octokit.rest.repos
-  .listForOrg({
-    org: "octokit",
-    type: "public",
-  })
-  .then(({ data }) => {
-    // handle data
-  });
+const getMethods = (obj: object) => Object.getOwnPropertyNames(obj);
+const makeKeywords = (obj: object, name: string) =>
+  getMethods(obj).map((method) => `${name}.${method}`);
+
+const sleep = (msec: number) =>
+  new Promise((resolve) => setTimeout(resolve, msec));
+
+async function main() {
+  const octokit = new Octokit({ auth: process.env.GITHUB_PERSONAL_TOKEN });
+  const keywords = makeKeywords(Math, "Math");
+  const counts: { code: number; count: number }[] = [];
+
+  for (const keyword of keywords) {
+    const res = await octokit.search.code({
+      q: keyword,
+    });
+    console.log();
+    console.log(
+      `${keyword} ${res.data.total_count} [API remain ${res.headers["x-ratelimit-remaining"]} / 1min]`
+    );
+    counts.push({ code: res.status, count: res.data.total_count });
+
+    // 10 requests per minute (GitHub code search API)
+
+    await sleep(6.1 * 1000);
+  }
+  counts.sort((a, b) => b.count - a.count);
+  console.log(counts.map((c) => `${c.code},${c.count}`).join("\n"));
+}
+
+main();
