f2613b9d75b23f18786748a03b068687cf51b14f
elzup <guild0105@gmail.com>
2023-05-22 17:26:31 +0900

    refactor

--- main.ts
+++ main.ts
@@ -1,33 +1,39 @@
 import { Octokit } from "@octokit/rest";
-
 const getMethods = (obj: object) => Object.getOwnPropertyNames(obj);
 const makeKeywords = (obj: object, name: string) =>
   getMethods(obj).map((method) => `${name}.${method}`);
-
 const sleep = (msec: number) =>
   new Promise((resolve) => setTimeout(resolve, msec));
 
-async function main() {
-  const octokit = new Octokit({ auth: process.env.GITHUB_PERSONAL_TOKEN });
-  const keywords = makeKeywords(Math, "Math");
-  const counts: { code: number; count: number }[] = [];
+type Row = { code: number; count: number; tag: string };
 
+async function makeCountsData(
+  api: Octokit,
+  obj: object,
+  name: string,
+  tagConverter: (field: string) => string
+) {
+  const keywords = makeKeywords(obj, name);
+  const rows: Row[] = [];
   for (const keyword of keywords) {
-    const res = await octokit.search.code({
-      q: keyword,
-    });
-    console.log();
+    const res = await api.search.code({ q: keyword });
     console.log(
       `${keyword} ${res.data.total_count} [API remain ${res.headers["x-ratelimit-remaining"]} / 1min]`
     );
-    counts.push({ code: res.status, count: res.data.total_count });
-
+    rows.push({
+      code: res.status,
+      count: res.data.total_count,
+      tag: tagConverter(keyword),
+    });
     // 10 requests per minute (GitHub code search API)
-
     await sleep(6.1 * 1000);
   }
-  counts.sort((a, b) => b.count - a.count);
-  console.log(counts.map((c) => `${c.code},${c.count}`).join("\n"));
+  rows.sort((a, b) => b.count - a.count);
+  console.log(rows.map((c) => `${c.code},${c.count}`).join("\n"));
 }
 
+async function main() {
+  const octokit = new Octokit({ auth: process.env.GITHUB_PERSONAL_TOKEN });
+  await makeCountsData(octokit, Math, "Math", (field) => field);
+}
 main();
